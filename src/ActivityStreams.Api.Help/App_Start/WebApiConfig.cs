abb28de90448bc41871d791df32b6058c4c4160c
mentormate-kalinvenkov <kalin.venkov@mentormate.com>
2015-09-18 17:39:51 +0300

    Chnage ActivityStreams.Api.Help.csproj to target 4.5 framework and be empty web api project

--- src/ActivityStreams.Api.Help/App_Start/WebApiConfig.cs
+++ src/ActivityStreams.Api.Help/App_Start/WebApiConfig.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Http.Description;
 
 namespace ActivityStreams.Api.Help
@@ -10,26 +7,16 @@ namespace ActivityStreams.Api.Help
     {
         public static void Register(HttpConfiguration config)
         {
-
             var theApiConfig = new HttpConfiguration(); // New config object to represent your API's config
             Api.WebApiConfig.Register(theApiConfig); // Configure via WebApiConfig in your API DLL
             theApiConfig.EnsureInitialized();
 
             config.Services.Replace(typeof(IApiExplorer), new ApiExplorer(theApiConfig));
 
-
-            // Web API configuration and services
-
-            // Web API routes
-            //config.MapHttpAttributeRoutes();
-
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{action}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-
-
+                defaults: new { id = RouteParameter.Optional });
         }
     }
 }
--- src/ActivityStreams.Api.Help/Properties/AssemblyInfo.cs
+++ src/ActivityStreams.Api.Help/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ActivitySteams.Api.Help")]
+[assembly: AssemblyTitle("ActivityStreams.Api.Help")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ActivitySteams.Api.Help")]
+[assembly: AssemblyProduct("ActivityStreams.Api.Help")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bcd00b62-6886-4831-8490-a77ffd331e26")]
+[assembly: Guid("f94daf68-d59f-4843-98d5-621448c999d8")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
