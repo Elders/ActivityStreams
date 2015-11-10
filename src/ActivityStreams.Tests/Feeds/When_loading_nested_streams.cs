9bedc7a4d6297ebc5410fd134db16b28a9f95e7a
mynkow <mynkow@gmail.com>
2015-11-10 18:17:25 +0200

    Make all integration tests to use the sandbox modifier

--- src/ActivityStreams.Tests/Feeds/When_loading_nested_streams.cs
+++ src/ActivityStreams.Tests/Feeds/When_loading_nested_streams.cs
@@ -9,11 +9,11 @@ namespace ActivityStreams.Tests.Feeds
     {
         Establish context = () =>
         {
-            streamService.Attach(Encoding.UTF8.GetBytes("top"), Encoding.UTF8.GetBytes("str1-top_str_id"));
-            streamService.Attach(Encoding.UTF8.GetBytes("nested_level_1"), Encoding.UTF8.GetBytes("str2-nested_level_1"));
+            streamService.Attach(Encoding.UTF8.GetBytes("top" + sandbox), Encoding.UTF8.GetBytes("str1-top_str_id" + sandbox));
+            streamService.Attach(Encoding.UTF8.GetBytes("nested_level_1" + sandbox), Encoding.UTF8.GetBytes("str2-nested_level_1" + sandbox));
         };
 
-        It should_return_only_top_level_streams = () => streamService.Get(Encoding.UTF8.GetBytes("top")).Streams.Count().ShouldEqual(1);
+        It should_return_only_top_level_streams = () => streamService.Get(Encoding.UTF8.GetBytes("top" + sandbox)).Streams.Count().ShouldEqual(1);
     }
 }
 
--- src/ActivityStreams.Tests/Feeds/When_stream_is_detached.cs
+++ src/ActivityStreams.Tests/Feeds/When_stream_is_detached.cs
@@ -10,12 +10,12 @@ namespace ActivityStreams.Tests.Feeds
     {
         Establish context = () =>
         {
-            streamService.Attach(Encoding.UTF8.GetBytes("top"), Encoding.UTF8.GetBytes("attached-level_1"));
+            streamService.Attach(Encoding.UTF8.GetBytes("top" + sandbox), Encoding.UTF8.GetBytes("attached-level_1" + sandbox));
         };
 
-        Because of = () => streamService.Detach(Encoding.UTF8.GetBytes("top"), Encoding.UTF8.GetBytes("attached-level_1"), DateTime.UtcNow);
+        Because of = () => streamService.Detach(Encoding.UTF8.GetBytes("top" + sandbox), Encoding.UTF8.GetBytes("attached-level_1" + sandbox), DateTime.UtcNow);
 
-        It should_not_remove_the_detached_stream = () => streamService.Get(Encoding.UTF8.GetBytes("top")).Streams.Count().ShouldEqual(1);
+        It should_not_remove_the_detached_stream = () => streamService.Get(Encoding.UTF8.GetBytes("top" + sandbox)).Streams.Count().ShouldEqual(1);
         It should_have_timestamp_when_detach_happened;
     }
 }
