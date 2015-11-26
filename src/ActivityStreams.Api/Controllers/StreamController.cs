a81a3294a11623a841fbb6792117d4694cfb6825
mentormate-kalinvenkov <kalin.venkov@mentormate.com>
2015-11-26 14:33:48 +0200

    Removed AttachStreamWithExpirationDate and added expiresAt with default value to AttachStream

--- src/ActivityStreams.Api/Controllers/StreamController.cs
+++ src/ActivityStreams.Api/Controllers/StreamController.cs
@@ -14,32 +14,19 @@ namespace ActivityStreams.Api.Controllers
         /// </summary>
         /// <param name="streamId"></param>
         /// <param name="toStreamId"></param>
+        /// <param name="expiresAt">Expiration date and time. Default is DateTime.MaxValue</param>
         /// <returns>IHttpActionResult</returns>
         /// <remarks>Attaching stream to stream</remarks>
         [HttpPost]
-        public IHttpActionResult AttachStream(string streamId, string toStreamId)
+        public IHttpActionResult AttachStream(string streamId, string toStreamId, DateTime? expiresAt = null)
         {
+            if (expiresAt.HasValue == false)
+                expiresAt = DateTime.FromFileTimeUtc(ActivityStream.DefaultExpirationTimestamp);
+
+
             var streamIdBytes = Encoding.UTF8.GetBytes(streamId);
             var toStreamIdBytes = Encoding.UTF8.GetBytes(toStreamId);
-            WebApiApplication.StreamService.Attach(toStreamIdBytes, streamIdBytes);
-
-            return Ok();
-        }
-
-        /// <summary>
-        /// Attach stream to stream with expiration date
-        /// </summary>
-        /// <param name="streamId"></param>
-        /// <param name="toStreamId"></param>
-        /// <param name="expiresAt"></param>
-        /// <returns>IHttpActionResult</returns>
-        /// <remarks>Attaching stream to stream with expiration date</remarks>
-        [HttpPost]
-        public IHttpActionResult AttachStreamWithExpirationDate(string streamId, string toStreamId, DateTime expiresAt)
-        {
-            var streamIdBytes = Encoding.UTF8.GetBytes(streamId);
-            var toStreamIdBytes = Encoding.UTF8.GetBytes(toStreamId);
-            WebApiApplication.StreamService.Attach(toStreamIdBytes, streamIdBytes, expiresAt);
+            WebApiApplication.StreamService.Attach(toStreamIdBytes, streamIdBytes, expiresAt.Value);
 
             return Ok();
         }
@@ -49,7 +36,7 @@ namespace ActivityStreams.Api.Controllers
         /// </summary>
         /// <param name="streamId"></param>
         /// <param name="fromStreamId"></param>
-        /// <param name="detachSince"></param>
+        /// <param name="detachSince">Detach date and time</param>
         /// <returns></returns>
         [HttpPost]
         public IHttpActionResult DetachStream(string streamId, string fromStreamId, DateTime detachSince)
