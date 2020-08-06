b00ba4fefd2c9341a8d6ef00398c54351719196c
AlexPlom <alexiosplomaritis@gmail.com>
2020-08-06 16:15:08 +0300

    Removes dependency to msvcrt.dll for comparing sequences

--- src/ActivityStreams/Helpers/ByteArrayHelper.cs
+++ src/ActivityStreams/Helpers/ByteArrayHelper.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Runtime.InteropServices;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ActivityStreams.Helpers
 {
@@ -7,38 +7,17 @@ namespace ActivityStreams.Helpers
     {
         public bool Equals(byte[] x, byte[] y)
         {
-            return ByteArrayHelper.Compare(x, y);
+            return ((ReadOnlySpan<byte>)x).SequenceCompareTo(y) == 0;
         }
 
         public int GetHashCode(byte[] obj)
         {
-            return ByteArrayHelper.ComputeHash(obj);
+            return ComputeHash(obj);
         }
 
         private static ByteArrayEqualityComparer instance = new ByteArrayEqualityComparer();
 
         public static ByteArrayEqualityComparer Default { get { return instance; } }
-    }
-
-    public static class ByteArrayHelper
-    {
-        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
-        static extern int memcmp(byte[] b1, byte[] b2, long count);
-
-        public static bool Compare(byte[] b1, byte[] b2)
-        {
-            // Validate buffers are the same length.
-            // This also ensures that the count does not exceed the length of either buffer.  
-            return b1.Length == b2.Length && memcmp(b1, b2, b1.Length) == 0;
-        }
-
-        public static byte[] Combine(byte[] first, byte[] second)
-        {
-            byte[] rv = new byte[first.Length + second.Length];
-            System.Buffer.BlockCopy(first, 0, rv, 0, first.Length);
-            System.Buffer.BlockCopy(second, 0, rv, first.Length, second.Length);
-            return rv;
-        }
 
         public static int ComputeHash(params byte[] data)
         {
